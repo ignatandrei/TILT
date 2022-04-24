2cd440a63d529f1fd58653edb567c5487e5929aa
Andrei Ignat <ignatandrei@yahoo.com>
2022-04-24 17:19:22 +0300

    feat: adding versioning

--- src/backend/Net6/NetTilt/NetTilt/NetTilt.WebAPI/globals.cs
+++ src/backend/Net6/NetTilt/NetTilt/NetTilt.WebAPI/globals.cs
@@ -6,3 +6,4 @@ global using NetCore2BlocklyNew;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
 global using NetTilt.WebAPI;
+[assembly: AMS_Base.VersionReleased(Name = "FutureRelease", ISODateTime = "9999-04-16", recordData = AMS_Base.RecordData.Merges)]
