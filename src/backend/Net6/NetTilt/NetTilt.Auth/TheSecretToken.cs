486a0671f67d0e03a635934cbe10ef878922c637
Andrei Ignat <ignatandrei@yahoo.com>
2022-04-25 19:38:28 +0300

    feat: added secret

---
+++ src/backend/Net6/NetTilt/NetTilt.Auth/TheSecretToken.cs
@@ -0,0 +1,10 @@
+﻿namespace NetTilt.Auth;
+
+public class TheSecretToken
+{
+    public TheSecret(IConfiguration configuration)
+    {
+        Secret = configuration["MySettings:secretToken" ?? "andrei";
+    }
+    public string Secret { get; }
+}
--- src/backend/Net6/NetTilt/NetTilt.Auth/globals.cs
+++ src/backend/Net6/NetTilt/NetTilt.Auth/globals.cs
@@ -3,3 +3,4 @@ global using Generated;
 global using Microsoft.IdentityModel.Tokens;
 global using System.IdentityModel.Tokens.Jwt;
 global using System.Security.Claims;
+global using Microsoft.Extensions.Configuration;
