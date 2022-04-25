9ff1a994ebe32f6efef8cad32feee063f784f3be
Andrei Ignat <ignatandrei@yahoo.com>
2022-04-25 19:26:36 +0300

    feat: added jwt

--- src/backend/Net6/NetTilt/NetTilt.Auth/AuthUrl.cs
+++ src/backend/Net6/NetTilt/NetTilt.Auth/AuthUrl.cs
@@ -1,6 +1,4 @@
-﻿using Generated;
-
-namespace NetTilt.Auth;
+﻿namespace NetTilt.Auth;
 public class AuthUrl
 {
     private readonly ISearchDataTILT_URL search;
@@ -15,14 +13,30 @@ public class AuthUrl
         if (data == null)
             return null;
 
-        if(data .Length != 0)
+        if(data.Length != 0)
             return null;
 
         var item = data[0]!;
         if (item.Secret != secret)
             return null;
 
-        return item.ID.ToString();
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes("andrei");
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Webpage,item.ID.ToString() ),
+                    new Claim(ClaimTypes.Role, "Editor")
+                }),
+
+            Expires = DateTime.UtcNow.AddMinutes(5),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var ret=  tokenHandler.WriteToken(token);
+        return ret;
     }
 
 }
---
+++ src/backend/Net6/NetTilt/NetTilt.Auth/globals.cs
@@ -0,0 +1,5 @@
+﻿global using System.Text;
+global using Generated;
+global using Microsoft.IdentityModel.Tokens;
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
