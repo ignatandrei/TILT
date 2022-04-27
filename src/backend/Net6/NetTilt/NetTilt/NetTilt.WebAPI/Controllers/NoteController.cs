db753b9aa383f060151a589547e31c0b5c049954
Andrei Ignat <ignatandrei@yahoo.com>
2022-04-28 00:49:11 +0300

    feat: see notes and post

--- src/backend/Net6/NetTilt/NetTilt/NetTilt.WebAPI/Controllers/NoteController.cs
+++ src/backend/Net6/NetTilt/NetTilt/NetTilt.WebAPI/Controllers/NoteController.cs
@@ -23,14 +23,29 @@ public class NoteController : ControllerBase
         note.IDURL = idUrl.Value;
         note.ID = 0;
         note.ForDate = DateTime.UtcNow;
-        await insert.InsertTILT_Note(note);
+        var noteOrig = new TILT_Note();
+        noteOrig.CopyFrom(note);
+        await insert.InsertTILT_Note(noteOrig);
+        note.CopyFrom(noteOrig);
         return note;
 
     }
     [Authorize(Policy = "CustomBearer", Roles = "Editor")]
-    [HttpGet("{number}")]
-    public async Task<ActionResult<TILT_Note_Table[]?>> MyLastNotes(long number,[FromServices] ISearchDataTILT_Note searchNotes)
+    [HttpGet()]
+    public async Task<ActionResult<bool?>> HasPostedToday([FromServices] ISearchDataTILT_Note searchNotes)
     {
+        //Tobemoved
+        var all = await AllMyNotes(searchNotes);
+        if (all?.Value == null)
+            return false;
+        var now = DateTime.UtcNow.Date;
+        return all.Value.FirstOrDefault (it=>it.ForDate.HasValue && now.Subtract(it.ForDate.Value.Date).TotalDays == 0) !=null;
+    }
+    [Authorize(Policy = "CustomBearer", Roles = "Editor")]
+    [HttpGet()]
+    public async Task<ActionResult<TILT_Note_Table[]?>> AllMyNotes([FromServices] ISearchDataTILT_Note searchNotes)
+    {
+        //Tobemoved
         var c = this.User?.Claims.ToArray();
         var idUrl = auth.MainUrlId(c);
         if (idUrl == null)
@@ -38,7 +53,8 @@ public class NoteController : ControllerBase
             return new UnauthorizedResult();
         }
         var data =await searchNotes.TILT_NoteSimpleSearch_IDURL(SearchCriteria.Equal, idUrl.Value).ToArrayAsync();
-        return data;
+        var ret= data.Select(it => { var n = new TILT_Note_Table(); n.CopyFrom(it);return n; }).ToArray();
+        return ret;
     }
 }
 
