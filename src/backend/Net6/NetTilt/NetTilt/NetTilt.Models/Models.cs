aa29acbe4c1bc9e9d92c908b1f683eb45eee487e
Andrei Ignat <ignatandrei@yahoo.com>
2022-04-30 01:17:19 +0300

    feat: added activity to methods

--- src/backend/Net6/NetTilt/NetTilt/NetTilt.Models/Models.cs
+++ src/backend/Net6/NetTilt/NetTilt/NetTilt.Models/Models.cs
@@ -1,7 +1,15 @@
-﻿namespace NetTilt.Models
+﻿using System.Diagnostics;
+
+namespace NetTilt.Models
 {
     public partial class Models
     {
-
+        static ActivitySource activitySource = new ActivitySource("TILT_SOURCE");
+        public static IDisposable? StartMethod(string nameClass, string nameMethod)
+        {
+            return activitySource.StartActivity($"{nameClass}_{nameMethod}", ActivityKind.Client, default(ActivityContext));
+            //return null;
+        }
+        
     }
 }
