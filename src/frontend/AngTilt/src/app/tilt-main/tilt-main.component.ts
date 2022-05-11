9e97c01592d1b98e731aa0770b936420e6bdc435
Andrei Ignat <ignatandrei@yahoo.com>
2022-05-11 22:46:19 +0300

    feat: identify myself

--- src/frontend/AngTilt/src/app/tilt-main/tilt-main.component.ts
+++ src/frontend/AngTilt/src/app/tilt-main/tilt-main.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -7,63 +7,90 @@ import { ShepherdService } from 'angular-shepherd';
 @Component({
   selector: 'app-tilt-main',
   templateUrl: './tilt-main.component.html',
-  styleUrls: ['./tilt-main.component.css']
+  styleUrls: ['./tilt-main.component.css'],
 })
-export class TiltMainComponent implements AfterViewInit  {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+export class TiltMainComponent implements AfterContentInit {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver, private shepherdService: ShepherdService) {}
-  ngAfterViewInit(): void {
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private shepherdService: ShepherdService
+  ) {}
+  ngAfterContentInit(): void {
+    
+    console.log(document.getElementsByClassName('publicUrls'));
+    // this.shepherdService.requiredElements = [
+    //   {
+    //     selector: '.publicUrls',
+    //     message: 'No search results found. Please execute another search, and try to start the tour again.',
+    //     title: 'No results'
+    //   }
+    // ];
     this.shepherdService.defaultStepOptions = {
       classes: 'custom-class-name-1 custom-class-name-2',
       scrollTo: false,
       cancelIcon: {
-        enabled: true
+        enabled: true,
       },
       highlightClass: 'highlight',
-        
+
       buttons: [
         {
           classes: 'shepherd-button-secondary',
           text: 'Exit',
+          action(){
+            return this.cancel();
+          }
         },
         {
           classes: 'shepherd-button-primary',
           text: 'Back',
           action() {
             return this.back();
-          }
+          },
         },
         {
           classes: 'shepherd-button-primary',
           text: 'Next',
           action() {
             return this.next();
-          }
-        }
-        
-              ]
-    };;
+          },
+        },
+      ],
+    };
     this.shepherdService.modal = true;
     this.shepherdService.confirmCancel = false;
-    
+
     this.shepherdService.addSteps([
-        {
-          id: 'intro',
-          attachTo: { 
-            element: '.first-element', 
-            on: 'bottom'
-          },
-        title: 'Welcome to TILT!',
-        text: ['Things I Learned Today'],
+      {
+        id: 'intro',
+        title: 'TILT!',
+        text: ['Welcome to Thing I Learnt  Today  =  TILT'],
+      },
+      {
+        id: 'publicUrls',
+        attachTo: {
+          element: '.publicUrls',
+          on: 'right',
+        },
+        title: 'TILT!',
+        text: ['here you can find all public urls'],
+      },
+      {
+        id: 'publicUrl',
+        attachTo: {
+          element: 'a[name="ignatandrei"]',
+          on: 'right',
+        },
+        title: 'TILT!',
+        text: ['Click on one- mine is ignatandrei'],
       }
     ]);
     this.shepherdService.start();
   }
-
 }
