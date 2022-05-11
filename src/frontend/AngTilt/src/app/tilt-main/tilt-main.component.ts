a2e7e936643275741faf7a487abdadbd65e020f4
Andrei Ignat <ignatandrei@yahoo.com>
2022-05-11 21:19:01 +0300

    feat: adding shepherd.js

--- src/frontend/AngTilt/src/app/tilt-main/tilt-main.component.ts
+++ src/frontend/AngTilt/src/app/tilt-main/tilt-main.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { ShepherdService } from 'angular-shepherd';
 
 @Component({
   selector: 'app-tilt-main',
   templateUrl: './tilt-main.component.html',
   styleUrls: ['./tilt-main.component.css']
 })
-export class TiltMainComponent {
+export class TiltMainComponent implements AfterViewInit  {
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -16,6 +17,53 @@ export class TiltMainComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private shepherdService: ShepherdService) {}
+  ngAfterViewInit(): void {
+    this.shepherdService.defaultStepOptions = {
+      classes: 'custom-class-name-1 custom-class-name-2',
+      scrollTo: false,
+      cancelIcon: {
+        enabled: true
+      },
+      highlightClass: 'highlight',
+        
+      buttons: [
+        {
+          classes: 'shepherd-button-secondary',
+          text: 'Exit',
+        },
+        {
+          classes: 'shepherd-button-primary',
+          text: 'Back',
+          action() {
+            return this.back();
+          }
+        },
+        {
+          classes: 'shepherd-button-primary',
+          text: 'Next',
+          action() {
+            return this.next();
+          }
+        }
+        
+              ]
+    };;
+    this.shepherdService.modal = true;
+    this.shepherdService.confirmCancel = false;
+    
+    this.shepherdService.addSteps([
+        {
+          id: 'intro',
+          attachTo: { 
+            element: '.first-element', 
+            on: 'bottom'
+          },
+        title: 'Welcome to TILT!',
+        text: ['Things I Learned Today'],
+      }
+    ]);
+    this.shepherdService.start();
+  }
 
 }
