d69ac329a3da354fbe84d5aa67134b659d4d46d7
Andrei Ignat <ignatandrei@yahoo.com>
2022-05-24 23:00:24 +0300

    feat: show text

--- src/frontend/AngTilt/src/app/one-public-tilt/one-public-tilt.component.ts
+++ src/frontend/AngTilt/src/app/one-public-tilt/one-public-tilt.component.ts
@@ -2,10 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { CalendarEvent } from 'angular-calendar';
-import { filter, map, switchMap, tap } from 'rxjs';
+import { startOfDay } from 'date-fns';
+import { filter, map, Subject, switchMap, tap } from 'rxjs';
 import { TILT } from '../classes/TILT';
 import { PublicTiltsService } from '../services/public-tilts.service';
 
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3',
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF',
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA',
+  },
+};
 @Component({
   selector: 'app-one-public-tilt',
   templateUrl: './one-public-tilt.component.html',
@@ -13,8 +28,8 @@ import { PublicTiltsService } from '../services/public-tilts.service';
 })
 export class OnePublicTiltComponent implements OnInit {
 
-
-  
+  activeDayIsOpen: boolean = true;
+  refresh = new Subject<void>();
   viewDate: Date = new Date();
   events: CalendarEvent[] = [];
 
@@ -29,6 +44,10 @@ export class OnePublicTiltComponent implements OnInit {
   }
   get tiltsFormArray(): FormArray{
     return this.profileForm.get('publicTILTS') as FormArray;
+}
+
+dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    this.viewDate = date;
 }
 
   ngOnInit(): void {
@@ -47,9 +66,19 @@ export class OnePublicTiltComponent implements OnInit {
         this.tiltsFormArray.clear();
         it=it.sort((a,b)=>b.forDate!.localeCompare(a.forDate!));
         // this.tiltsFormArray.push(...it.map(it=>this.fb.control(it)));
-        it.forEach(it=>this.tiltsFormArray.push(this.fb.control(it)));
+        it.forEach(a=>this.tiltsFormArray.push(this.fb.control(a)));
         // this.tiltsFormArray.push(new TILT());
-        
+
+        it.forEach(a=> this.events.push(
+          {
+            start:  startOfDay(a.LocalDate),
+            title: a.text||'',
+            color: colors.red,
+            allDay: true,
+            draggable: false
+          }
+        ));
+          this.refresh.next();
       }
     );
   }
