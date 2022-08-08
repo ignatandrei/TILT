17b66fe85531af585e9ebac1bf945296f4025c64
Andrei Ignat <ignatandrei@yahoo.com>
2022-08-08 21:22:45 +0300

    feat: added tilt

--- src/frontend/AngTilt14/src/app/app-routing.module.ts
+++ src/frontend/AngTilt14/src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import {
+  RouterModule,
+  RouterStateSnapshot,
+  Routes,
+  TitleStrategy,
+} from '@angular/router';
 import { LoginUrlGuard } from './login-url.guard';
 import { LoginUrlComponent } from './login-url/login-url.component';
 import { MyTiltComponent } from './my-tilt/my-tilt.component';
@@ -8,16 +13,46 @@ import { PublicTiltsComponent } from './public-tilts/public-tilts.component';
 import { TiltMainComponent } from './tilt-main/tilt-main.component';
 
 const routes: Routes = [
-
-  { path: 'tilt/public', component: PublicTiltsComponent, title: 'List of public tilts' },
+  {
+    path: 'tilt/public',
+    component: PublicTiltsComponent,
+    title: 'List of public tilts',
+  },
   { path: 'tilt/public/:id', component: OnePublicTiltComponent },
   { path: '', redirectTo: '/tilt/public', pathMatch: 'full' },
-  { path: 'tilt/my', component:MyTiltComponent , canActivate: [LoginUrlGuard], title:'My tilts' },
-  { path:'loginURL', component: LoginUrlComponent, title:"Login to add a tilt"}
+  {
+    path: 'tilt/my',
+    component: MyTiltComponent,
+    canActivate: [LoginUrlGuard],
+    title: 'My tilts',
+  },
+  {
+    path: 'loginURL',
+    component: LoginUrlComponent,
+    title: 'Login to add a tilt',
+  },
 ];
+
+@Injectable()
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      document.title = `TILT! - ${title}`;
+    } else {
+      var arr = routerState.url.split('/');
+      if(arr.length==0)
+        document.title = `TILT! - AAA`;
+      else
+        document.title = `TILT! - tilts for ` + arr[arr.length-1];
+    }
+  }
+}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{provide: TitleStrategy,  useClass: TemplatePageTitleStrategy}],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
