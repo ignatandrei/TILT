4c8c89fd567fe088853ec5d20a35e90a87ca608a
Andrei Ignat <ignatandrei@yahoo.com>
2022-08-08 21:13:57 +0300

    feat: added simple tilt

--- src/frontend/AngTilt14/src/app/app-routing.module.ts
+++ src/frontend/AngTilt14/src/app/app-routing.module.ts
@@ -9,11 +9,11 @@ import { TiltMainComponent } from './tilt-main/tilt-main.component';
 
 const routes: Routes = [
 
-  { path: 'tilt/public', component: PublicTiltsComponent },
+  { path: 'tilt/public', component: PublicTiltsComponent, title: 'List of public tilts' },
   { path: 'tilt/public/:id', component: OnePublicTiltComponent },
   { path: '', redirectTo: '/tilt/public', pathMatch: 'full' },
-  { path: 'tilt/my', component:MyTiltComponent , canActivate: [LoginUrlGuard] },
-  { path:'loginURL', component: LoginUrlComponent}
+  { path: 'tilt/my', component:MyTiltComponent , canActivate: [LoginUrlGuard], title:'My tilts' },
+  { path:'loginURL', component: LoginUrlComponent, title:"Login to add a tilt"}
 ];
 
 @NgModule({
